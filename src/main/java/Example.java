dd31e7c8a7d27c52e2c1375b69b3b2d2922d2863
Michaël Schmutz <michael@mbp-de-michael.home>
2018-03-22 19:21:50 +0000

    First commit

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -14,31 +14,11 @@ public class Example {
         DummyNodeChild tesla = new DummyNodeChild("tesla", "modelS");
         DummyNodeParent baeldung = new DummyNodeParent("baeldung");
 
-        baeldung.setCar(tesla);
+        baeldung.setChild(tesla);
         session.save(baeldung);
     }
 
-
-//    private void printGreeting() {
-//        try ( Session session = driver.session() )
-//        {
-//            String greeting = session.writeTransaction( new TransactionWork<String>()
-//            {
-//                @Override
-//                public String execute( Transaction tx )
-//                {
-//                    StatementResult result = tx.run( "CREATE (a:Greeting) " +
-//                                    "SET a.message = $message " +
-//                                    "RETURN a.message + ', from node ' + id(a)",
-//                            parameters( "message", "hello, world") );
-//                    return result.single().get( 0 ).asString();
-//                }
-//            } );
-//            System.out.println( greeting );
-//        }
-//    }
-
     public static void main( String... args ) {
-        Example greeter = new Example();
+        new Example();
     }
 }
